bf2093eaf12248f36bc423e9177c675c416b25ca
Jakub Słota <jakub.slota@interia.pl>
2025-04-22 20:59:46 +0200

    dodanie mappingu dla pojedynczego usera i wszystkich userow, tworzenie bazy danych automatycznie

---
+++ user-service/src/main/java/com/bookstore/user_service/services/UserService.java
@@ -0,0 +1,25 @@
+package com.bookstore.user_service.services;
+
+import com.bookstore.user_service.entities.User;
+import com.bookstore.user_service.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}
