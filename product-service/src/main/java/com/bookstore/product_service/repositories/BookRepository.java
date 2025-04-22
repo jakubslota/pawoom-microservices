f3f97ba88fe415f615531e8d740ddc2e2f8baac7
Jakub Słota <jakub.slota@interia.pl>
2025-04-22 22:53:46 +0200

    product service2

--- product-service/src/main/java/com/bookstore/product_service/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.product_service.repositories;
-
-import com.bookstore.product_service.entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
