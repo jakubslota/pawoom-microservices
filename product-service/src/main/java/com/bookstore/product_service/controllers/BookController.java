2faa51cf6edd8f6c64c03d63101618141590fb27
Jakub Słota <jakub.slota@interia.pl>
2025-04-22 22:49:31 +0200

    product service

---
+++ product-service/src/main/java/com/bookstore/product_service/controllers/BookController.java
@@ -0,0 +1,40 @@
+package com.bookstore.product_service.controllers;
+
+import com.bookstore.product_service.entities.Book;
+import com.bookstore.product_service.services.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> save(@RequestBody Book book) {
+        return ResponseEntity.ok(bookService.save(book));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> findAll() {
+        return ResponseEntity.ok(bookService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> findById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(bookService.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        bookService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ product-service/src/main/java/com/bookstore/product_service/entities/Book.java
@@ -0,0 +1,72 @@
+package com.bookstore.product_service.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+    private double price;
+    private int quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
+
+
