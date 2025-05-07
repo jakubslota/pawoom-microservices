30a0d65b8e72efe4c0eb97cf0207906b4b22256f
Jakub Słota <jakub.slota@interia.pl>
2025-05-07 11:03:38 +0200

    api gateway, change routes to products

--- product-service/src/main/java/com/bookstore/product_service/controllers/BookController.java
+++ product-service/src/main/java/com/bookstore/product_service/controllers/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/products")
 public class BookController {
 
     private final BookService bookService;
